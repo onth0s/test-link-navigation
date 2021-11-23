269e8f0e8a93142177cae709e325ed7165c183ea
onth0s <metaagnosis@gmail.com>
2021-11-24 00:13:39 +0100

    almost there...

--- src/App.js
+++ src/App.js
@@ -8,18 +8,41 @@ function App() {
 	return (<div className="container mx-auto">
 		<NavLinkBar
 			links={[
-				'home',
-				'shop',
-				'something else',
-				'"·$%&/()',
-				'checkout',
+				{
+					name: 'home',
+					route: '/'
+				},
+				{
+					name: 'shop',
+					route: '/shop'
+				},
+				{
+					name: 'checkout',
+					route: '/checkout'
+				},
+				{
+					name: 'home',
+					route: '/'
+				},
+				{
+					name: 'shop',
+					route: '/shop'
+				},
+				{
+					name: 'checkout',
+					route: '/checkout'
+				},
 			]}
 		/>
 
-		<div className="border-2 border-white rounded-lg p-4">
+		<div className="border-2 border-white rounded-lg p-4 mx-4">
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/shop" element={<Shop />} />
+
+				<Route path="*" element={<div>
+					<p>404: No match for this route</p>
+				</div>} />
 			</Routes>
 		</div>
 	</div>);
--- src/components/NavLinkBar/index.js
+++ src/components/NavLinkBar/index.js
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router";
+
 export const NavLinkBar = ({ links = [] }) => {
-	return (<div className="m-12 bg-gray-800 text-2xl h-20 flex space-x-4 items-center px-4">
+	return (<div className="mt-12 mb-6 mx-4 bg-gray-800 text-xl h-14 flex space-x-4 items-center px-4 overflow-auto">
 		{links.map((el, i) => (
-			<InnerLink text={el} hideArrow={i == links.length - 1} />
+			<InnerLink key={i} text={el.name} hideArrow={i === links.length - 1} route={el.route} />
 		))}
 	</div>);
 }
 
-const InnerLink = ({ text, hideArrow }) => {
+const InnerLink = ({ text, hideArrow, route }) => {
+	const navigate = useNavigate();
 	const arrow = <i className="transform scale-75 fas mt-1 fa-chevron-right" />;
 
-	return (<div className="flex space-x-2 bg-gray-600_ items-center cursor-pointer">
+	const handleLink = () => navigate(route);
+
+	return (<div className="flex space-x-2 bg-gray-600_ items-center cursor-pointer"
+		onClick={handleLink}
+	>
 		<p className="capitalize">{text}</p>
 		{!hideArrow && arrow}
 	</div>);
