19c88f3a1d4ea7ad1ced10cc27d44b4b7cae441a
onth0s <metaagnosis@gmail.com>
2021-11-23 08:14:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="container bg-blue-200">
+			Completely clean and empty
+			<span className="mx-1 italic">create-react-app</span>
+			template.
+		</div>
+	);
+}
+
+export default App;
