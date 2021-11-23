9ad8431006b5ead8e73318574bbfaaa58f7fc843
onth0s <metaagnosis@gmail.com>
2021-11-24 00:23:20 +0100

    added shop categories

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,5 +1,20 @@
+const categories = [
+	'Computers',
+	'Phones',
+	'Games',
+	'Books',
+];
+
 export const Shop = () => {
-	return (<div>
-		SHOP
+	return (<div className="bg-yellow-900_ grid grid-cols-2 gap-4">
+		{categories.map((el, i) => (
+			<Category key={i} text={el} />
+		))}
+	</div>);
+}
+
+const Category = ({ text }) => {
+	return (<div className="cursor-pointer border-2 border-blue-700 rounded-md py-4 flex items-center justify-center">
+		<p>{text}</p>
 	</div>);
 }
